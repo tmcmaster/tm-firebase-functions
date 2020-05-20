452ccb2ac0f17e900c87032674f630f05d59253d
Tim McMaster <tim@mcmaster.id.au>
2020-05-20 23:06:55 +1000

    cleanup

--- demo/index.js
+++
@@ -1,2 +0,0 @@
-import '../src/index.js';
-import '../docs/main.js';
--- docs/index.js
+++
@@ -1,3 +0,0 @@
-import './web_modules/@wonkytech/tm-examples.js';
-import './web_modules/@wonkytech/tm-firebase-functions.js';
-import './main.js';
